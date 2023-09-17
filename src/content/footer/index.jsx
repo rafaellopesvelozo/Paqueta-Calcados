2320614b1911fefcb558e3789420d0b7e5f5477a
rafaellopesvelozo <rafaellopesvelozo@gmail.com>
2023-09-16 22:24:02 -0300

    enterpriseFooter

--- src/content/footer/index.jsx
+++ src/content/footer/index.jsx
@@ -1,68 +1,76 @@
 import React from "react";
 import * as styled from "./styles";
+import logoFooter from "./../../assets/Footer/logo-footer.jpg";
 const Footer = () => {
   return (
     <styled.ConteinerFooter>
-      <section>
-        <h6>institucional</h6>
-        <ul>
-          <li>Quem somos</li>
-          <li>Nossas Lojas</li>
-          <li>Soluções Corporativas</li>
-          <li>Política de privacidade</li>
-          <li>Contrato de Compra e Venda</li>
-          <li>Crédito pessoal</li>
-        </ul>
-      </section>
-      <section>
-        <h6>Ajuda e Suporte</h6>
-        <ul>
-          <li>Troca e Devolução</li>
-          <li>2° Via do Boleto</li>
-          <li>Detalhamento da Fatura</li>
-          <li>Entrega</li>
-          <li>Meus Pedidos</li>
-          <li>Dúvidas Frequentes</li>
-          <li>Status do Pedido</li>
-        </ul>
-      </section>
-      <section>
-        <div>
-          <h6>Atendimento</h6>
-          <p>Fale Conosco</p>
-        </div>
-        <div>
-          <h6>Endereço</h6>
-          <address>Rua Antonio Frederico Ozanan, 2601</address>
-          <address>Canoas - RS | CNPJ: 01.098.983/0175-02</address>
-        </div>
-        <div>
-          <h6>Trabalhe conosco</h6>
-          <p>Venha crescer com a Paquetá</p>
-        </div>
-      </section>
-      <section  className="payment">
-        <div>
-          <h6>Formas de Pagamento</h6>
-        </div>
-        <div className="social-media">
-          <h6>Redes Sociais</h6>
-          <div className="social-media-icons">
-            <i class="bi bi-instagram"></i>
-            <i class="bi bi-facebook"></i>
-            <i class="bi bi-twitter"></i>
-            <i class="bi bi-youtube"></i>
-            <i class="bi bi-pinterest"></i>
+      <div>
+        <section>
+          <h6>institucional</h6>
+          <ul>
+            <li>Quem somos</li>
+            <li>Nossas Lojas</li>
+            <li>Soluções Corporativas</li>
+            <li>Política de privacidade</li>
+            <li>Contrato de Compra e Venda</li>
+            <li>Crédito pessoal</li>
+          </ul>
+        </section>
+        <section>
+          <h6>Ajuda e Suporte</h6>
+          <ul>
+            <li>Troca e Devolução</li>
+            <li>2° Via do Boleto</li>
+            <li>Detalhamento da Fatura</li>
+            <li>Entrega</li>
+            <li>Meus Pedidos</li>
+            <li>Dúvidas Frequentes</li>
+            <li>Status do Pedido</li>
+          </ul>
+        </section>
+        <section className="footer-service">
+          <div className="footer-talk-us">
+            <h6>Atendimento</h6>
+            <p>Fale Conosco</p>
           </div>
-        </div>
-      </section>
-      <section className="awards-security">
-        <div>
-          <h6>Prêmios e Certifições</h6>
-        </div>
-        <div>
-          <h6>Segurança</h6>
-        </div>
+          <div className="footer-adress">
+            <h6>Endereço</h6>
+            <address>Rua Antonio Frederico Ozanan, 2601</address>
+            <address>Canoas - RS | CNPJ: 01.098.983/0175-02</address>
+          </div>
+          <div className="footer-work-us">
+            <h6>Trabalhe conosco</h6>
+            <p>Venha crescer com a Paquetá</p>
+          </div>
+        </section>
+        <section className="payment">
+          <div>
+            <h6>Formas de Pagamento</h6>
+          </div>
+          <div className="social-media">
+            <h6>Redes Sociais</h6>
+            <div className="social-media-icons">
+              <i class="bi bi-instagram"></i>
+              <i class="bi bi-facebook"></i>
+              <i class="bi bi-twitter"></i>
+              <i class="bi bi-youtube"></i>
+              <i class="bi bi-pinterest"></i>
+            </div>
+          </div>
+        </section>
+        <section className="awards-security">
+          <div>
+            <h6>Prêmios e Certifições</h6>
+          </div>
+          <div>
+            <h6>Segurança</h6>
+          </div>
+        </section>
+      </div>
+
+      <section className="footer-group">
+        <h6>UMA EMPRESA DO GRUPO</h6>
+        <img src={logoFooter} alt="logo paquetá footer" />
       </section>
     </styled.ConteinerFooter>
   );
--- src/content/footer/styles.js
+++ src/content/footer/styles.js
@@ -1,56 +1,80 @@
 import styled from "styled-components";
 
 export const ConteinerFooter = styled.footer`
-  padding: 2% 10%;
-  display: flex;
-  justify-content: space-between;
-  background-color: #ebebeb;
-
-  .payment,
-  .awards-security {
+  div {
+    padding: 2% 10%;
     display: flex;
-    flex-direction: column;
     justify-content: space-between;
-  }
-
-  section {
-    address,
-    p {
-      color: #808080;
-    }
-
-    h6 {
-      font-size: 16px;
-      font-weight: 700;
-      font-family: "Montserrat", sans-serif;
-      margin-bottom: 10px;
-      color: #383838;
-    }
-
-    ul {
+    background-color: #ebebeb;
+    .payment,
+    .awards-security {
       display: flex;
       flex-direction: column;
-      gap: 10px;
-      li {
-        font-size: 14px;
-        font-weight: 400;
-        font-family: "Montserrat", sans-serif;
-      }
+      justify-content: space-between;
     }
 
-    .social-media {
-      .social-media-icons {
+    section {
+      div {
+        padding: 0;
+      }
+      .footer-service{
         display: flex;
-        gap: 15px;
+        flex-direction: column;
+        justify-content: space-between;
+      }
+      address,
+      p {
+        color: #808080;
+      }
 
-        .bi-instagram,
-        .bi-facebook,
-        .bi-twitter,
-        .bi-youtube,
-        .bi-pinterest {
-          font-size: 25px;
+      h6 {
+        font-size: 16px;
+        font-weight: 700;
+        font-family: "Montserrat", sans-serif;
+        margin-bottom: 10px;
+        color: #383838;
+      }
+
+      ul {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        li {
+          font-size: 14px;
+          font-weight: 400;
+          font-family: "Montserrat", sans-serif;
         }
       }
+      .footer-adress,
+      .footer-talk-us,
+      .footer-work-us {
+        display: block;
+      }
+      .social-media {
+        .social-media-icons {
+          display: flex;
+          gap: 15px;
+
+          .bi-instagram,
+          .bi-facebook,
+          .bi-twitter,
+          .bi-youtube,
+          .bi-pinterest {
+            font-size: 25px;
+          }
+        }
+      }
+    }
+  }
+  .footer-group {
+    background-color: #383838;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px 0;
+    gap: 10px;
+    h6 {
+      color: #ffffff;
     }
   }
 `;
